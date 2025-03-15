e395802aa5a6fde75364e2594e9f07d0a319cea3
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2025-03-15 11:43:27 -0700

    Frontend
    Estructura de trabajo para recibir datos en el frontend con Django.

--- filtering/views.py
+++ filtering/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import UploadFileForm
 
 # Create your views here.
+def upload_file(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES["file"]
+            # TODO: Procesar el archivo aqui
+            return render(request, "upload.html", {"form": form, "success": True})
+        else:
+            form = UploadFileForm()
+            
+        return render(request, "upload.html", {'form': form})
+    
