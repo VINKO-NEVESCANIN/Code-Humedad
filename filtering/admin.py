ce7a4e2767b356e12bee45e98059b600c6034665
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2025-03-21 21:30:08 -0700

    Update admin.py

--- filtering/admin.py
+++ filtering/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ArchivoExcel, RegistroTemperatura
 
 # Register your models here.
+admin.site.register(ArchivoExcel)
+admin.site.register(RegistroTemperatura)
+
